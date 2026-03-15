481b5af66288144c3a2caf2210de5e81ed333e43
Vasili Raminashvili <vaska.r02@gmail.com>
2026-03-15 17:12:52 +0400

    install filament and theme

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 'resources/js/app.js',
+                'resources/views/**'
+            ],
             refresh: true,
         }),
         tailwindcss(),
