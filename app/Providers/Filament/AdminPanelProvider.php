7652ee9dac9a402081c567c9f3b6ff2fb8f985d1
Vaso <vaska.r02@gmail.com>
2026-03-19 17:45:41 +0400

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('')
             ->login()
             ->colors([
                 'primary' => Color::Indigo,
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
