1bc58ebd82033c8004c10d55b4096274368551b3
Vaso <vaska.r02@gmail.com>
2026-03-25 20:12:31 +0400

    wip

---
+++ config/filament-clear-cache.php
@@ -0,0 +1,22 @@
+<?php
+
+use CmsMulti\FilamentClearCache\Http\Livewire\ClearCache;
+
+return [
+    // Command to run when clearing the cache
+    'default_commands' => [
+        'optimize',
+    ],
+
+    // Session name for the indicator count
+    'changes_count' => 'filament-clear-cache',
+
+    // Livewire component for clear cache button in header.
+    'livewireComponentClass' => ClearCache::class,
+
+    // Permissions check
+    'permissions' => false,
+
+    // Role check
+    'role' => false,
+];
