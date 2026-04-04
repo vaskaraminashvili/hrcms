a06ab9459eca394a308199f34350fa8edf3b01b3
Vaso <vaska.r02@gmail.com>
2026-04-04 15:56:28 +0400

    import poistions

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\EmployeeImportService;
+use App\Services\PositionImportService;
 use Illuminate\Http\Request;
 
 class ImportController extends Controller
@@ -10,6 +11,15 @@ class ImportController extends Controller
     public function importEmployees(Request $request, EmployeeImportService $employeeImportService)
     {
         $result = $employeeImportService->importAll(
+            clearTableBefore: $request->boolean('clear', true)
+        );
+
+        return response()->json($result);
+    }
+
+    public function importPositions(Request $request, PositionImportService $positionImportService)
+    {
+        $result = $positionImportService->importAll(
             clearTableBefore: $request->boolean('clear', true)
         );
 
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ImportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/import-employees', [ImportController::class, 'importEmployees'])->name('import.employees');
+Route::get('/import-positions', [ImportController::class, 'importPositions'])->name('import.positions');
