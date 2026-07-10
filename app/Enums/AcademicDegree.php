45f09c43b68032f156bde8814d3255a1ece8bd9c
Vaso <vaska.r02@gmail.com>
2026-07-10 16:54:28 +0400

    correct enums

--- app/Enums/AcademicDegree.php
+++ app/Enums/AcademicDegree.php
@@ -5,14 +5,12 @@ namespace App\Enums;
 enum AcademicDegree: string
 {
     case DOCTOR = 'DOCTOR';
-    case MAGISTER = 'MAGISTER';
     case OTHER = 'OTHER';
 
     public function getLabel(): string
     {
         return match ($this) {
             self::DOCTOR => 'დოქტორი',
-            self::MAGISTER => 'მაგისტრი',
             self::OTHER => 'სხვა',
         };
     }
@@ -21,7 +19,6 @@ enum AcademicDegree: string
     {
         return match ($this) {
             self::DOCTOR => 'primary',
-            self::MAGISTER => 'secondary',
             self::OTHER => 'gray',
         };
     }
